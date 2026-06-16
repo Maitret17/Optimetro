84da77e472a00f9e38978003f9233d77b6a39e9e
Maitret17 <tombieleg@gmail.com>
2026-06-16 12:25:10 +0200

    Typo

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, RATEfrei");
+        System.out.println("Hello, RATEFREI");
         Graph graph = new Graph();
 
         // Line A / red
