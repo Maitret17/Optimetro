b40ab585605662437ac3af7635455901d140a41e
Maitret17 <tombieleg@gmail.com>
2026-07-02 14:48:12 +0200

    Fonction pour afficher le trajet sur la carte

--- src/main/java/com/optimetro/model/StationNode.java
+++ src/main/java/com/optimetro/model/StationNode.java
@@ -4,13 +4,17 @@ public class StationNode {
     private String id;
     private String name;
     private String line;
+    private Double latitude;
+    private Double longitude;
     private int groupId; // For later usage on dijkstra
 
-    public StationNode(String id, String name, String line, int groupId) {
+    public StationNode(String id, String name, String line, int groupId, Double latitude, Double longitude) {
         this.id = id;
         this.name = name;
         this.line = line;
         this.groupId = groupId;
+        this.latitude = latitude;
+        this.longitude = longitude;
     }
 
     public String getId() {
@@ -24,6 +28,10 @@ public class StationNode {
     public String getLine() {
         return line;
     }
+
+    public Double getLatitude() { return latitude; }
+
+    public Double getLongitude() { return longitude; }
 
     @Override
     public boolean equals(Object obj) {
